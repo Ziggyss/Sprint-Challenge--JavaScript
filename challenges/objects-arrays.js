31307319fc760270edc09d32f391f6d1ab36b76e
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-08-23 08:52:57 +0100

    complete array tasks

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -73,7 +73,12 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array.  
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
+
 const universities = [];
+graduates.forEach(function(data){
+  universities.push(data.university)
+});
+universities.sort();
 console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
@@ -83,12 +88,18 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+graduates.forEach(function(data) {
+  contactInfo.push(data.first_name + ' ' + data.email);
+});
 console.log(contactInfo);
-
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+uni = graduates.filter(function uni (data){
+  return data.university.includes("Uni")
+});
 console.log(uni);
+
 
 
 // ==== ADVANCED Array Methods ====
